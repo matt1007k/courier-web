da1a2b4f8de57c02ac6406686b15fd428711041c
matt1007k <matt1007k@gmail.com>
2021-05-28 20:31:01 -0500

    Test va

--- promo_codes/views.py
+++ promo_codes/views.py
@@ -11,15 +11,10 @@ def validate_code_view(request):
     code = request.GET.get('code')
     promo_code = PromoCode.objects.get_valid(code)
 
-    return JsonResponse({
-        'status': True,
-        'promocode': promo_code.code,
-        'order_id': order.pk
-    }, status=200)
-    # if promo_code is None:
-    #     return JsonResponse({
-    #         'status': False
-    #     }, status=404)
+    if promo_code is None:
+        return JsonResponse({
+            'status': False
+        }, status=404)
 
     # if promo_code.discount > 0:
     #     order.apply_promo_code(promo_code)
@@ -30,6 +25,13 @@ def validate_code_view(request):
     #     order.apply_promo_code_special(promo_code)
     #     total_previous = order.get_total_previous_special()
     #     discount = order.get_discount_special()
+
+    return JsonResponse({
+        'status': True,
+        'is_discount': promo_code.discount > 0,
+        'is_special': promo_code.special > 0,
+        'order_id': order.pk
+    }, status=200)
 
     # return JsonResponse(data={
     #     'status': True,
